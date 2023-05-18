d4406fd476f9122fe7ae722e09605db6bb7dcac9
NARUTO9556 <gruver5263@gmail.com>
2023-05-18 17:39:04 +0800

    промежуточный коммит

--- src/Employee.java
+++ src/Employee.java
@@ -1,26 +1,32 @@
 public class Employee {
-    private String employeeFirstName;
-    private String employeeMiddleName;
-    private String employeeLastName;
+    private final String employeeFullName;
+    private int department;
     private int salary;
-    static int counter = 1;
-    int id = counter++;
-        public Employee(String employeeLastName, String employeeFirstName, String employeeMiddleName) {
-        this.employeeLastName = employeeLastName;
-        this.employeeFirstName = employeeFirstName;
-        this.employeeMiddleName = employeeMiddleName;
+    private static int counter = 1;
+    private final int id;
+
+    public Employee(String employeeFullName, int department, int salary, int id) {
+        this.employeeFullName = employeeFullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = getCounter();
+        counter++;
     }
 
-    public String getEmployeeFirstName() {
-        return employeeFirstName;
+    public String getEmployeeFullName() {
+        return employeeFullName;
     }
 
-    public String getEmployeeMiddleName() {
-        return employeeMiddleName;
+    public int getDepartment() {
+        return department;
     }
 
-    public String getEmployeeLastName() {
-        return employeeLastName;
+    public static int getCounter() {
+        return counter;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public int getSalary() {
@@ -30,9 +36,16 @@ public class Employee {
     public void setSalary(int salary) {
         this.salary = salary;
     }
-    @Override
-    public String toString() {
-        return "Сотрудник = '"+ employeeLastName + ' ' + employeeFirstName + ' ' + employeeMiddleName + '\'';
+
+    public void setDepartment(int department) {
+        this.department = department;
     }
 
+    @Override
+    public String toString() {
+        return "ФИО сотрудника ='" + employeeFullName + '\'' +
+                ", Отдел = " + department +
+                ", зарплата = " + salary +
+                ", id=" + id;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,15 @@
-import java.util.Arrays;
-
 public class Main {
+    public static Employee[] employeeList = new Employee[10];
     public static void main(String[] args) {
-        int[] arr = generateRandomArray();
-        System.out.println(Arrays.toString(arr));
-        printSeparator();
-        String[] Employees = new String[10];
-        Employees[0] = String.valueOf(new Employee("Иванов", "Иван", "Иванович"));
-        Employees[1] = String.valueOf(new Employee("Иванов", "Иван", "Иванович"));
-        Employees[2] = String.valueOf(new Employee("Иванов", "Иван", "Иванович"));
-        Employees[3] = String.valueOf(new Employee("Иванов", "Иван", "Иванович"));
-        Employees[4] = String.valueOf(new Employee("Иванов", "Иван", "Иванович"));
-        Employees[5] = String.valueOf(new Employee("Иванов", "Иван", "Иванович"));
-        Employees[6] = String.valueOf(new Employee("Иванов", "Иван", "Иванович"));
-        Employees[7] = String.valueOf(new Employee("Иванов", "Иван", "Иванович"));
-        Employees[8] = String.valueOf(new Employee("Иванов", "Иван", "Иванович"));
-        Employees[9] = String.valueOf(new Employee("Иванов", "Иван", "Иванович"));
-        System.out.println(Arrays.toString(Employees));
+        employeeList[0] = new Employee("Иванов Иван Иванович", 1, 25000, 1);
+        System.out.println(employeeList[0]);
+        employeeList[1] = new Employee("Степанов Борис Андреевич", 2, 27000, 2);
+        System.out.println(employeeList[1]);
         printSeparator();
 
 
     }
     public static void printSeparator() {
         System.out.println("====================");
-    }
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(50_000) + 50_000;
-        }
-        return arr;
     }
 }
